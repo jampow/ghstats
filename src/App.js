7daac50db332ca2bbaf774d6bcc2bf8ec993c653
Gianpaulo M. Soares <jam_pow@hotmail.com>
2019-01-23 17:43:26 -0200

    feat: translating to portuguese

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edite <code>src/App.js</code> e salve para recarregar.
           </p>
           <a
             className="App-link"
