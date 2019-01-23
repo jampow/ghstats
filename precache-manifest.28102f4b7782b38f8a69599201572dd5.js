026ca0313a246cd9b372d0fcde290578d4dfa052
Travis (from Travis CI) <no@email.com>
2019-01-23 19:45:15 +0000

    Deploy jampow/ghstats to github.com/jampow/ghstats.git:gh-pages

---
+++ precache-manifest.28102f4b7782b38f8a69599201572dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghstats/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3de41b348c651696932",
+    "url": "/ghstats/static/js/runtime~main.c3de41b3.js"
+  },
+  {
+    "revision": "5b49863127bdcf76a119",
+    "url": "/ghstats/static/js/main.5b498631.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ghstats/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5b49863127bdcf76a119",
+    "url": "/ghstats/static/css/main.5748ca80.chunk.css"
+  },
+  {
+    "revision": "9abe39ba8deb0247c3b45bab33ba5ffa",
+    "url": "/ghstats/index.html"
+  }
+];
